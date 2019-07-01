694231d8e887eb5f3e29c27d340fb612a40e2791
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 21:46:20 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -21,11 +21,12 @@ function showRepositories() {
   document.getElementById("repositories").innerHTML = repoList;
 }
 
-function getCommits(anchor) {
+function getCommits(el) {
+  const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
-  req.open("get", `https://api.github.com/repos/octocat/#{anchor.dataset.repo}/commits`);
-  req.send;
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
 }
 
 function showCommits() {

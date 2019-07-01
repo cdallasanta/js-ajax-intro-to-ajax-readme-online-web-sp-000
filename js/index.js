5f9a2fa8819ddabdee18929cea13fab8bb388935
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 21:56:50 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -30,14 +30,14 @@ function getCommits(anchor) {
 
 function showCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${
-    commits.map(commit =>{
+  const commitsList = `<ul>${commits
+    .map(commit =>
         '<li><strong>' +
         commit.author.login +
         '</strong> - ' +
         commit.commit.message +
         '</li>'
-    }).join('')
+    ).join('')
   }</ul>`;
   document.getElementById('commits').innerHTML = commitsList;
 }

f7330a4ee32f5cfeb9a62805fac4b5c78db0d725
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 21:17:44 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // your code here
 function getRepositories(){
   const req = new XMLHttpRequest();
+  req.addEventListener('load', )
   req.open('get', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
